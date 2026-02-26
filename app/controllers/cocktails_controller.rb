18a5d9c146729e52328b2e5f54ebf2dceef59853
jaharbaugh <harbaugh.ja@gmail.com>
2026-02-26 13:00:17 -0600

    Create basic Chart.js scatter plot to act as timeline visualization

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,6 +4,16 @@ class CocktailsController < ApplicationController
             .includes(:publication)
             .joins(:publication)
             .order("publications.publication_year ASC")
+        @timeline_data = @cocktails.map do |cocktail|
+            {
+                name: cocktail.name,
+                x: cocktail.publication.publication_year,
+                y: 0,
+                title: cocktail.publication.title,
+                author: cocktail.publication.author,
+                spirit: cocktail.base_spirit
+            }
+        end
     end
     def show
         @cocktail = Cocktail.find(params[:id])
