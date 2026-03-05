540e234cc12ee4e5361b9ea288bcff72b5173425
jaharbaugh <harbaugh.ja@gmail.com>
2026-03-05 16:21:09 -0600

    Update UI of chart.js

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,17 @@
 class CocktailsController < ApplicationController
+    SPIRIT_COLORS = {
+        "gin" => "rgb(70,200,120)",
+        "vodka" => "rgb(120,200,255)",
+        "rum" => "rgb(0,180,170)",
+        "bourbon" => "rgb(220,140,60)",
+        "rye" => "rgb(200,110,60)",
+        "scotch" => "rgb(150,120,200)",
+        "tequila" => "rgb(120,220,90)",
+        "mezcal" => "rgb(240,150,70)",
+        "brandy_cognac" => "rgb(236,202,152)",
+        "other" => "rgb(180,180,180)"
+    }
+
     def index
         @cocktails = Cocktail
             .includes(:publication)
@@ -12,11 +25,15 @@ class CocktailsController < ApplicationController
                 y: 0,
                 title: cocktail.publication.title,
                 author: cocktail.publication.author,
-                spirit: cocktail.base_spirit
+                spirit: cocktail.base_spirit,
+                color: SPIRIT_COLORS[cocktail.base_spirit]
+
             }
         end
     end
     def show
         @cocktail = Cocktail.find(params[:id])
+        @photo = @cocktail.photo
+        
     end
 end
